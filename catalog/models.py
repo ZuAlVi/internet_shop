d389dc4d8ad38fccee3694ad34f0145fda652ec3
Alexandr Zubarev <zualvi@yandex.ru>
2023-11-28 14:57:20 +0300

    Changed catalog/models.py

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.db import connection
 
-# Create your models here.
+NULLABLE = {
+    'blank': True,
+    'null': True,
+}
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=50, verbose_name='Категория')
+    description = models.CharField(max_length=250, verbose_name='Описание', **NULLABLE)
+
+    def __str__(self):
+        return self.category_name
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    @classmethod
+    def truncate_table_restart_id(cls):
+        with connection.cursor() as cursor:
+            cursor.execute(f'TRUNCATE TABLE {cls._meta.db_table} RESTART IDENTITY CASCADE')
+
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=50, verbose_name='Название')
+    description = models.CharField(max_length=100, verbose_name='Описание', **NULLABLE)
+    preview = models.ImageField(upload_to='product/', verbose_name='Превью', **NULLABLE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
+    price = models.IntegerField(verbose_name='Цена')
+    date_make = models.DateTimeField(verbose_name='Дата создания', **NULLABLE)
+    date_changed = models.DateTimeField(verbose_name='Дата изменения', **NULLABLE)
+
+    def __str__(self):
+        return self.product_name
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+        ordering = ('product_name',)
