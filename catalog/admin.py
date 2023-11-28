ab8ae99b190db606afdb542108f56b649258ace6
Alexandr Zubarev <zualvi@yandex.ru>
2023-11-28 14:56:27 +0300

    Changed config/admin.py

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from catalog.models import Product, Category
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'product_name', 'price', 'category')
+    list_filter = ('category',)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'category_name')
+    
