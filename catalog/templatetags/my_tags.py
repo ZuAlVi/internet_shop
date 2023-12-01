6e9a8ca3585ddfb1fdf7afef69d67d0c012627c7
Alexandr Zubarev <zualvi@yandex.ru>
2023-12-01 17:53:08 +0300

    Updated project

--- catalog/templatetags/my_tags.py
+++ catalog/templatetags/my_tags.py
@@ -7,11 +7,13 @@ register = template.Library()
 def media_filter(value):
     if value:
         return f'/media/{value}'
-    return '#'
+
+    return '/static/img/No_photo.jpeg'
 
 
 @register.simple_tag()
 def media_tag(value):
     if value:
         return f'/media/{value}'
-    return '#'
+
+    return '/static/img/No_photo.jpeg'
